bbd002410b0d6963080f61472bb7ad98c748b093
nabazio <nabazio.enry@gmail.com>
2022-08-16 22:22:58 -0300

    console log indicando modificações do paulo

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
+console.log("Aplicação está se conectando ao banco de dados")
+//mais um console log de teste:
+console.log("O paulo fez esse commit sem voce ver na aula!")
